915fffce9ab6d057eaa523072207da95404e413e
Phani Vemuri <phanikumarvemuri@users.noreply.github.com>
2023-09-11 15:22:04 -0500

    Converting spark udf's to native functions (#26)
    * bug fix for table write
    * optimizing imports
    * removing udfs
    * updating comments for the function

--- spark_expectations/utils/udf.py
+++ spark_expectations/utils/udf.py
@@ -1,36 +1,31 @@
-from typing import List, Dict, Union
 from pyspark.sql import Column
-from pyspark.sql.functions import udf
-from pyspark.sql.types import ArrayType, MapType, StringType
+from pyspark.sql.functions import filter, size, transform, when, lit, array
 
 
-@udf(returnType=ArrayType(MapType(StringType(), StringType())))
-def remove_empty_maps(column: Column) -> List[Union[Dict[str, str], None]]:
+def remove_empty_maps(column: Column) -> Column:
     """
-    This Spark UDF takes a column of type array(map(str,str)) and removes empty maps from it
+    This function takes a column of type array(map(str,str)) and removes empty maps from it
     Args:
         column: Provide a column of type array(map(str,str))
         Returns:
-                list: Returns a list which is not having empty maps
+                list: Returns a Column which is not having empty maps
     """
     # The below line is already tested in test_udf.py but not shown in coverage. So ignoring it for now!
-    return [
-        x for x in column if isinstance(x, dict) and len(x) != 0
-    ]  # pragma: no cover
+    return filter(column, lambda x: size(x) > 0)  # pragma: no cover
 
 
-@udf(returnType=ArrayType(StringType()))
-def get_actions_list(column: Column) -> List[str]:
+def get_actions_list(column: Column) -> Column:
     """
-    This Spark UDF takes column of type array(map(str,str)) and creates list by picking action_if_failed from dict
+    This function takes column of type array(map(str,str)) and creates list by picking action_if_failed from dict
     Args:
         column: Provide a column of type array(map(str,str))
 
     Returns:
-           list: returns list of action_if_failed from the set expectations rules
+           list: returns a column with list of action_if_failed from the set expectations rules
 
     """
 
-    action_failed = [itr.get("action_if_failed") for itr in column]  # pragma: no cover
-
-    return action_failed if len(action_failed) > 0 else ["ignore"]  # pragma: no cover
+    action_if_failed = transform(column, lambda x: x["action_if_failed"])
+    return when(size(action_if_failed) == 0, array(lit("ignore"))).otherwise(
+        action_if_failed
+    )  # pragma: no cover
