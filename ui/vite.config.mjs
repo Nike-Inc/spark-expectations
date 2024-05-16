e4040a946bd1709132a27bafa999e2804e857f05
Sri Krishna Chekuri <cskcvarma13@gmail.com>
2024-05-16 08:20:23 -0400

    Initializing theme files (#11)
    * Adding a basic mantine theme file. It will be updated as the code base develops.
    
    Any colors or gaps will not be hardcoded.
    
    They should first be defined in the theme and then should be used in the code.
    
    * updating .gitignore

--- ui/vite.config.mjs
+++ ui/vite.config.mjs
@@ -1,43 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import istanbul from 'vite-plugin-istanbul';
 
 export default defineConfig({
-    plugins: [react(), tsconfigPaths(), istanbul({
-        include: [
-            'src/**/*.ts',
-            'src/**/*.tsx',
-        ],
-        exclude: [
-            'node_modules/**',
-            'tests/**',
-            '**/__*',
-            '**/*.mock.ts',
-            'test-utils/__mocks__/**',
-            'postcss.config.cjs',
-        ],
-        extension: [ '.ts', '.tsx' ],
-        cypress: false,
-        requireEnv: false
-    })],
+    plugins: [react(), tsconfigPaths()],
     test: {
         globals: true,
         environment: 'jsdom',
         setupFiles: './vitest.setup.mjs',
     },
     coverage: {
-        provider: 'v8',
+        provider: 'c8',
         reporter: ['text', 'html'],
-    },
-    server: {
-        proxy: {
-            '/login/oauth/access_token': {
-                target: 'https://github.com',
-                changeOrigin: true,
-                rewrite: path => path.replace(/^\/login\/oauth\/access_token/, '/login/oauth/access_token')
-
-            }
-        }
+        exclude: [
+            '**/index.ts',
+            '**/node_modules/**',
+            '**/tests/**'
+        ]
     }
 });
