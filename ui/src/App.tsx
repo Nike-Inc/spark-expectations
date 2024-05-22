523bbb8447089043470655424155b4fb4478a105
Sri Krishna Chekuri <cskcvarma13@gmail.com>
2024-05-22 18:07:21 -0400

    implemented notification from mantine (#14)

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,7 +1,7 @@
 import '@mantine/core/styles.css';
+import '@mantine/notifications/styles.css';
 import React, { FC } from 'react';
 import './App.css';
-// import { AppLayout } from '@/layouts';
 import { AppProvider } from '@/providers';
 
 export const App: FC = () => <AppProvider />;
