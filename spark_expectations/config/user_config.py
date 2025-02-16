d3356e189ddb1512fad18b1cf36163ddfe26fb2f
Sudeepta pal <111543327+sudeep7978@users.noreply.github.com>
2025-02-16 15:09:33 +0530

    updating config

--- spark_expectations/config/user_config.py
+++ spark_expectations/config/user_config.py
@@ -4,10 +4,27 @@ from dataclasses import dataclass
 @dataclass
 class Constants:
     # declare const user config variables for email notification
-    se_notifications_enable_email = "spark.expectations.notifications.email.enabled"
     se_notifications_enable_smtp_server_auth = (
         "spark.expectations.notifications.email.smtp_server_auth"
     )
+    se_notifications_smtp_password = "spark.expectations.notifications.smtp.password"
+    se_notifications_smtp_creds_dict = (
+        "spark.expectations.notifications.smtp.creds.dict"
+    )
+    cbs_smtp_password = "spark.expectations.notifications.cerberus.smtp.password"
+    dbx_smtp_password = "spark.expectations.notifications.dbx.smtp.password"
+
+
+    se_user_defined_custom_dataframe = "spark.expectations.user.defined.custom.dataframe"
+    se_notifications_enable_custom_dataframe = "spark.expectations.notifications.enable.custom.dataframe"
+    se_dq_obs_default_email_template = "spark.expectations.dq.obs.default.email.template"
+    se_dq_obs_mode_of_communication="spark.expectations.dq.obs.mode.of.communication"
+    se_notifications_service_account_email="spark.expectations.notifications.service.account.email"
+    se_dq_obs_alert_flag="spark.expectations.notifications.alert.flag.disable"
+    se_notifications_service_account_password="spark.expectations.notifications.service.account.password"
+    se_notifications_smtp_user_name = "spark.expectations.notifications.smtp.user.name"
+    se_notifications_enable_email = "spark.expectations.notifications.email.enabled"
+    se_enable_obs_dq_report_result = "spark.expectations.notifications.observability.enabled"
     se_notifications_enable_custom_email_body = (
         "spark.expectations.notifications.enable.custom.email.body"
     )
@@ -17,15 +34,9 @@ class Constants:
     se_notifications_email_smtp_port = (
         "spark.expectations.notifications.email.smtp_port"
     )
-    se_notifications_smtp_password = "spark.expectations.notifications.smtp.password"
-    se_notifications_smtp_creds_dict = (
-        "spark.expectations.notifications.smtp.creds.dict"
-    )
-    cbs_smtp_password = "spark.expectations.notifications.cerberus.smtp.password"
-    dbx_smtp_password = "spark.expectations.notifications.dbx.smtp.password"
     se_notifications_email_from = "spark.expectations.notifications.email.from"
     se_notifications_email_to_other_mail_id = (
-        "spark.expectations.notifications.email.to.other.mail.com"
+        'spark.expectations.notifications.email.to.other.mail.com'
     )
     se_notifications_email_subject = "spark.expectations.notifications.email.subject"
     se_notifications_email_custom_body = (
