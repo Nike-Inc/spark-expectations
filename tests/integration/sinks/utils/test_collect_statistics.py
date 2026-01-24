34cafbca7043f3fbb3f4281cecb8ff59d4a922b5
rvprimer <rvprimer@gmail.com>
2026-01-23 23:18:19 -0800

    Fix test_collect_statistics: add mock attributes for get_dbr_workspace_id and get_dbr_workspace_url

--- tests/integration/sinks/utils/test_collect_statistics.py
+++ tests/integration/sinks/utils/test_collect_statistics.py
@@ -568,6 +568,8 @@ def test_collect_stats_on_success_failure(
     )
     setattr(_mock_context, "get_se_streaming_stats_dict", {"se.streaming.enable": True})
     setattr(_mock_context, "get_topic_name", "dq-sparkexpectations-stats")
+    setattr(_mock_context, "get_dbr_workspace_id", "local")
+    setattr(_mock_context, "get_dbr_workspace_url", "local")
 
     _mock_context.spark = spark
     _mock_context.product_id = "product1"
@@ -1092,6 +1094,8 @@ def test_collect_stats_on_success_failure_exception(
     )
     setattr(_mock_context, "get_se_streaming_stats_dict", {"se.streaming.enable": True})
     setattr(_mock_context, "get_topic_name", "dq-sparkexpectations-stats")
+    setattr(_mock_context, "get_dbr_workspace_id", "local")
+    setattr(_mock_context, "get_dbr_workspace_url", "local")
 
     writer = SparkExpectationsWriter(_mock_context)
     statistics_writer_obj = SparkExpectationsCollectStatistics(_mock_context, writer)
